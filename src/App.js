8ab0e4a2893c5087a9db013228e47a2b7406c9ee
yash <yaswanth.k23@gmail.com>
2022-04-02 12:44:15 +0530

    react-routing

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
-import CategoryDir from "./components/category-directory/category-dir.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./rotues/home/home.component";
+import Navigation from "./rotues/navigation/navigation.component";
 
-function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
+const App = () => {
   return (
-    <div>
-      <CategoryDir categories={categories} />
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
   );
-}
+};
 
 export default App;
---
+++ src/rotues/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <React.Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </React.Fragment>
+  );
+};
+
+export default Navigation;
