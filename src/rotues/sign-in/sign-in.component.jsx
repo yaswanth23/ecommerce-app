8553d25f7b2da710d03fd5933b7bf2bce027f117
yash <yaswanth.k23@gmail.com>
2022-04-03 17:58:05 +0530

    sign in with redirect changes

--- src/rotues/sign-in/sign-in.component.jsx
+++ src/rotues/sign-in/sign-in.component.jsx
@@ -1,17 +1,35 @@
 import {
+  auth,
   signInWithGooglePopup,
+  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firbase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
 
 const SignIn = () => {
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getRedirectResult(auth);
+      if (response) {
+        const userDocRef = await createUserDocumentFromAuth(response.user);
+      }
+    }
+    fetchData();
+  }, []);
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h2>Sign in page</h2>
       <button onClick={logGoogleUser}>Sign in with google</button>
+      {/* <button onClick={signInWithGoogleRedirect}>
+        Sign in with google redirect
+      </button> */}
     </div>
   );
 };
