2525907d6e80f8dfe97c78cb7e8ce8fb917b55bb
yaswanth <yaswanth.k23@gmail.com>
2022-04-10 16:52:53 +0530

    created check out page and its functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./rotues/home/home.component";
 import Navigation from "./rotues/navigation/navigation.component";
 import Authentication from "./rotues/authentication/authentication.component";
 import Shop from "./shop/shop.component";
+import Checkout from "./rotues/checkout/checkout.component";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
---
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+import "./checkout-item.styles.scss";
+
+const CheckoutItem = ({ cartItem }) => {
+  const { clearItemFromCart, addItemToCart, removeItemToCart } =
+    useContext(CartContext);
+  const { name, imageUrl, price, quantity } = cartItem;
+  return (
+    <div className="checkout-item-container">
+      <div className="image-container">
+        <img src={imageUrl} alt={`${name}`} />
+      </div>
+      <span className="name">{name}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItemToCart(cartItem)}>
+          &#8722;
+        </div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItemToCart(cartItem)}>
+          &#43;
+        </div>
+      </span>
+      <span className="price">{price}</span>
+      <div
+        className="remove-button"
+        onClick={() => clearItemFromCart(cartItem)}
+      >
+        &#10006;
+      </div>
+    </div>
+  );
+};
+
+export default CheckoutItem;
