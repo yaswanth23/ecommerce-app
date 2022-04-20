0d2524af02a24613e84c2202aab78181d164177b
yaswanth <yaswanth.k23@gmail.com>
2022-04-20 16:46:20 +0530

    usage of redux persist

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import { Provider } from "react-redux";
 import App from "./App";
 //import { CategoriesProvider } from './contexts/categories.context';
 //import { CartProvider } from "./contexts/cart.context";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.scss";
 
@@ -15,13 +16,15 @@ const rootElement = document.getElementById("root");
 render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        {/* <CategoriesProvider> */}
-        {/* <CartProvider> */}
-        <App />
-        {/* </CartProvider> */}
-        {/* </CategoriesProvider> */}
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          {/* <CategoriesProvider> */}
+          {/* <CartProvider> */}
+          <App />
+          {/* </CartProvider> */}
+          {/* </CategoriesProvider> */}
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   rootElement
